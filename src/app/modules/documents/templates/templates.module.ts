e2eb7d54d67b6ff20dcfb7532c794726d1565309
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-08-20 16:14:31 +0530

    RecipientModule and DragDropModule added

--- src/app/modules/documents/templates/templates.module.ts
+++ src/app/modules/documents/templates/templates.module.ts
@@ -8,6 +8,9 @@ import { iEditorModule } from "esigndoccontrol";
 import { EdittemplateComponent } from './edittemplate/edittemplate.component';
 import { FileUploadModule } from 'primeng/fileupload';
 import {CardModule} from 'primeng/card';
+import {RecipientModule} from '../../../shared/usercontrol/recipient/recipient.module';
+import {DragDropModule} from 'primeng/dragdrop';
+
 @NgModule({
   declarations: [AddComponent, ViewComponent, EdittemplateComponent],
   imports: [
@@ -16,7 +19,9 @@ import {CardModule} from 'primeng/card';
     SharedModule,
     iEditorModule,
     CardModule,
-    FileUploadModule
+    FileUploadModule,
+    RecipientModule,
+    DragDropModule
     
   ]
 })
