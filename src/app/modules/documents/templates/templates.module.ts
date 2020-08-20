e142b7c6646a2380de6a034fc24281465dfeb70b
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-08-20 18:22:53 +0530

    removed iEditorModule

--- src/app/modules/documents/templates/templates.module.ts
+++ src/app/modules/documents/templates/templates.module.ts
@@ -4,7 +4,7 @@ import { AddComponent } from './add/add.component';
 import { ViewComponent } from './view/view.component';
 import {TemplateRoutingModule} from '../templates/template-routing.module';
 import { SharedModule } from '../../../shared/shared.module';
-import { iEditorModule } from "esigndoccontrol";
+
 import { EdittemplateComponent } from './edittemplate/edittemplate.component';
 import { FileUploadModule } from 'primeng/fileupload';
 import {CardModule} from 'primeng/card';
@@ -19,7 +19,6 @@ import { DataViewModule } from 'primeng/dataview';
     CommonModule,
     TemplateRoutingModule,
     SharedModule,
-    iEditorModule,
     CardModule,
     FileUploadModule,
     RecipientModule,
