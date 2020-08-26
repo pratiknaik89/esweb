35264832d5728144f53580a531297cebbf9c4683
Chinmay Yogi <chinmay.yogi@ideas2tech.com>
2020-08-26 12:15:45 +0530

    envlop service created

---
+++ src/app/service/envolope.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { environment } from "../../environments/environment";
+import { GlobalService } from "./global.service";
+import { map } from "rxjs/operators";
+import { observable, Observable } from "rxjs";
+import { DataService } from '../service/dataservice-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvolopeService {
+
+  constructor(private dataservice: DataService, private globalservice: GlobalService) { }
+
+  getEnvolope(req: any) {
+    return this.dataservice.getHttpData('/envolope', req);
+}
+SaveEnvolope(req: any) {
+    return this.dataservice.postHttpData('/envolope', req);
+}
+
+}
+
+
