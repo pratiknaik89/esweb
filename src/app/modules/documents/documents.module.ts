7d307414e8382037cedff7b263c59fb905d9ec86
Pratik Naik <pratik@ideas2tech.com>
2020-09-19 19:42:05 +0530

    add support  for debugging

--- src/app/modules/documents/documents.module.ts
+++ src/app/modules/documents/documents.module.ts
@@ -5,7 +5,7 @@ import { DocumentsRoutingModule } from '../documents/documents-routing.module';
 import { from } from 'rxjs';
 import { EditorComponent } from './editor/editor.component';
 import { iEditorModule } from "esigndoccontrol";
-// import { iEditorModule } from "/Users/pratiknaik/Work/i2t/DocEditor/idoceditor/dist/esigndoccontrol";
+//import { iEditorModule } from "/Users/pratiknaik/Work/i2t/DocEditor/idoceditor/dist/esigndoccontrol";
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
